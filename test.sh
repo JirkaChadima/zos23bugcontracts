ebab229f93ebdc794c763d26c5457ad3f18a93b2
Jirka Chadima <chadima.jiri@gmail.com>
2019-06-04 10:23:22 +0200

    This crashes

--- test.sh
+++ test.sh
@@ -6,10 +6,9 @@ rm zos.dev*
 rm .zos.session
 
 
-./node_modules/.bin/zos init zos-repro-repo --no-interactive
+./node_modules/.bin/zos init zos-repro-repo
 ./node_modules/.bin/zos add Directory
 # npm run dev-net
 ./node_modules/.bin/zos session --network development --from 0x87265a62c60247f862b9149423061b36b460f4BB --expires 3600 --no-interactive
 ./node_modules/.bin/zos push --network development
 ./node_modules/.bin/zos create Directory --network development
-
